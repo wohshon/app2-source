5ceddd15b7a97be0b781635f19c1a16440a37d49
ws <wohshon@gmail.com>
2023-04-03 09:26:43 +0000

    simple firestore usecase

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 var express = require('express');
 
-const { initializeApp } = require('firebase/app');
-const { getFirestore, collection, getDocs } = require('firebase/firestore');
-
+const {Firestore} = require('@google-cloud/firestore');
 
 var router = express.Router();
 const version = process.env.VERSION || 'v1'
+const projectId = process.env.PROJECT_ID || ''
+const collectionId = process.env.COLLECTION_ID || ''
+// Create a new client
+// const firestore = new Firestore();
+/*
+var firestore = new Firestore({ projectId:
+  'your-project-id', keyFilename: '/path/to/keyfile.json'
+  });
+ */ 
+  var firestore = new Firestore({ projectId:
+    projectId
+  });
+let collectionRef = firestore.collection(collectionId);
+
 /* GET home page. */
 router.get('/app2/test', function(req, res, next) {
   const h = req.headers;
@@ -39,6 +51,37 @@ router.post('/app2', function(req,res,next){
   res.status(200).send(payload); 
 });
 
+router.get('/getdocs/:collectionId', function(req, res, next) {
+  let collectionId = req.params.collectionId;
+  let collectionRef = firestore.collection(collectionId);
+  getAllDocuments(collectionRef).then(documentSnapshots => {
+    /**debug 
+      for (let documentSnapshot of documentSnapshots) {
+         if (documentSnapshot.exists) {
+           console.log(`Found document with data: ${documentSnapshot.id}`);
+           console.log(`${JSON.stringify(documentSnapshot.data())}`)
+         } else {
+           console.log(`Found missing document: ${documentSnapshot.id}`);
+         }
+      }
+  */
+      //return documentSnapshots;
+      console.log(`got ${documentSnapshots.length} docs`);
+      let doc = {};
+      let docs = [];
+      for (let documentSnapshot of documentSnapshots) {
+        if (documentSnapshot.exists) {
+          doc.id = documentSnapshot.id;
+          doc.data = documentSnapshot.data();
+          docs.push(doc);
+        }
+      }
+      res.status(200).send(docs);
+    
+   });
+
+})
+
 //demo
 //curl -X POST -H "Content-type: application/json" -d '{"email":"hello", "txt":"123"}' localhost:8080/app2
 router.post('/app2/submit', function(req,res,next){
@@ -46,9 +89,49 @@ router.post('/app2/submit', function(req,res,next){
   console.log(payload);
   var len = Buffer.byteLength(payload.toString(),"utf-8");
   console.log(`${len} Bytes`);  
-  payload.message = "processed";
+  payload.message = "received";
   payload.timestamp = new Date();
-  res.status(200).send(payload); 
+  //save to firestore
+  add(payload).then(
+    () => {
+      let docs = getAllDocuments(collectionRef).then (
+        documentSnapshots => {
+          console.log(`got ${documentSnapshots.length} docs`);
+          let docs = [];
+          for (let documentSnapshot of documentSnapshots) {
+            let doc = {};
+            if (documentSnapshot.exists) {
+              doc.id = documentSnapshot.id;
+              doc.data = documentSnapshot.data();
+              docs.push(doc);
+            }
+          }
+          res.status(200).send(docs);
+        }
+      );    
+    }
+  );
 });
 
+
+
+async function add(payload) {
+  // Obtain a document reference.
+// Add a document with an auto-generated ID.
+  await collectionRef.add(payload).then((documentRef) => {
+    console.log(`Added document at ${documentRef.path})`);
+  });
+}
+
+async function getAllDocuments(collectionRef) {
+  return await collectionRef.listDocuments().then(documentRefs => {
+    if (documentRefs.length> 0) {
+      return firestore.getAll(...documentRefs);
+    }
+    else {
+      console.log("no documents")
+      return [];
+    } 
+ })
+}
 module.exports = router;
